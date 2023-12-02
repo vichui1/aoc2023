5a646f933202b79cfb9801be73b598e2f860318e
Victor Hui <huivictor01@gmail.com>
2023-12-02 13:56:58 +0100

    day1 and day2

---
+++ day1/day1.py
@@ -0,0 +1,29 @@
+with open("inputs/day1.txt") as f:
+    data = f.readlines()
+
+def calibration(data):
+    digits = [[int(i) for i in str if i.isdigit()] for str in data]
+    twoDigits = [ds[::len(ds)-1] if len(ds) >= 2 else ds+ds for ds in digits]
+    concatDigits = [int("".join([str(d) for d in ds])) for ds in twoDigits if ds]
+    return concatDigits
+
+part1 = sum(calibration(data))
+
+n1 = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+n2 = ["1one", "2two", "3three", "4four", "5five", "6six", "7seven", "8eight", "9nine"]
+numDict = dict(zip(n1, n2))
+
+def convStr(s):
+    word = ""
+    for c in s:
+        word += c
+        for n in n1:
+            if n in word:
+                s = s.replace(n, str(numDict[n]))  
+    return s
+
+cs = [convStr(l) for l in data]
+part2 = sum(calibration(cs))
+
+print("Part1: ", part1)
+print("Part2: ", part2)
---
+++ day2/day2.py
@@ -0,0 +1,54 @@
+import re
+
+with open("inputs/day2.txt") as f:
+    data = f.read().strip()
+
+ls = data.split("\n")
+gs = [l.replace(";", ",").split(",") for l in ls]
+
+def colorInSet(s, c):
+    if c in s:
+        return True
+    else:
+        return False
+
+def validSet(s):
+    cubes = int(re.findall("\d+", s)[-1])
+    if colorInSet(s,"red"):
+        if cubes > 12: return False
+    if colorInSet(s,"green"):
+        if cubes > 13: return False
+    if colorInSet(s,"blue"):
+        if cubes > 14: return False
+    return True
+
+validIds = []
+for i in range(1,len(gs)+1):
+    g = gs[i-1]
+    if all(map(validSet, g)):
+        validIds.append(i)
+
+part1 = sum(validIds)
+
+def maxCubes(rgb, s):
+    cubes = int(re.findall("\d+", s)[-1])
+    if colorInSet(s,"red"):
+        if cubes > rgb[0]: rgb[0] = cubes
+    if colorInSet(s,"green"):
+        if cubes > rgb[1]: rgb[1] = cubes
+    if colorInSet(s,"blue"):
+        if cubes > rgb[2]: rgb[2] = cubes
+    return rgb
+
+powerSum = 0
+for g in gs:
+    rgb = [1,1,1]
+    for s in g:
+        print(rgb)
+        rgb = maxCubes(rgb, s)
+    power = rgb[0] * rgb[1] * rgb [2]
+    powerSum += power
+
+part2 = powerSum
+print("Part1: ", part1)
+print("Part2: ", part2)
